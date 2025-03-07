6c4fac46f5f0f7a4b3e99495f0a662269b5565aa
hadi <hadi@hadi.com>
2025-03-07 16:45:00 +0330

    04 codding ...

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let firstName = "hadi",
-  lastName = "Raofian";
-lastName = null;
-firstName = undefined;
-let age = 25;
-let admin = true;
-console.log(
-  `first name: ${firstName} , last name: ${lastName} , age:  ${age} , admin: ${admin}`
-);
 
-// time 00-45-54-
+
+function sum(name, age){
+return console.log(`hello ${name} , you are ${age} years old`)
+}
+
+
+sum("ali", 20)
+console.log(resualt)
+
+
+
+// time 01-15-00-
