a2fdb3996799108bfbcc9adb0dfcda3acba8b042
hadi <hadi@hadi.com>
2025-03-07 14:06:18 +0330

    02 codding time 45

---
+++ script.js
@@ -0,0 +1,11 @@
+let firstName = "hadi",
+  lastName = "Raofian";
+lastName = null;
+firstName = undefined;
+let age = 25;
+let admin = true;
+console.log(
+  `first name: ${firstName} , last name: ${lastName} , age:  ${age} , admin: ${admin}`
+);
+
+// time 00-45-54
